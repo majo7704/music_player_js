f31b0827f0aed06d5c56588d22ae8b4582652deb
j m <jm@js-macbook-pro.home>
2020-03-27 16:23:43 +0100

    timer implemented

--- script.js
+++ script.js
@@ -100,3 +100,34 @@ progressContainer.addEventListener('click', setProgress);
 
 //Song ends
 audio.addEventListener('ended', nextSong);
+
+const timerWrapper = document.querySelector('.timer');
+const timer = document.querySelector('.timer span');
+const timerBar = document.querySelector('.timer div');
+//Time
+function setTime() {
+  let minutes = Math.floor(audio.currentTime / 60);
+  let seconds = Math.floor(audio.currentTime - minutes * 60);
+  let minuteValue;
+  let secondValue;
+
+  if (minutes < 10) {
+    minuteValue = '0' + minutes;
+  } else {
+    minuteValue = minutes;
+  }
+
+  if (seconds < 10) {
+    secondValue = '0' + seconds;
+  } else {
+    secondValue = seconds;
+  }
+
+  let audioTime = minuteValue + ':' + secondValue;
+  timer.textContent = audioTime;
+
+  let barLength = this.clientWidth * (audio.currentTime / audio.duration);
+  timerBar.style.width = barLength + 'px';
+}
+
+audio.addEventListener('timeupdate', setTime);
